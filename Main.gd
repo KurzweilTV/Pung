f675996acfa1af1c9d4680625b1947fc477c3602
Mike Barr <m.barr@mac.com>
2022-12-11 13:29:27 +0100

    Adds game ending

--- Main.gd
+++ Main.gd
@@ -2,18 +2,28 @@ extends Node2D
 
 func _ready() -> void:
 	randomize()
-	
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept") and $KickoffUI.visible:
+		game_start()
+		
 func game_start():
 	$KickoffUI.hide()
+	$Enviornment/Player1Score.text = str(Vars.P1Score)
+	$Enviornment/Player2Score.text = str(Vars.P2Score)
+	$Message/MessageLabel.hide()
 	yield(get_tree().create_timer(1), "timeout")
 	var coinflipx = [500, -500]
 	var rand_value_x = coinflipx[randi() % coinflipx.size()]
 	var coinflip_y = rand_range(-200, 200)
-	print(coinflip_y)
 	Vars.velocity = Vector2(rand_value_x, coinflip_y)
 	
-func game_end(): #TODO: Determin the winner and end the game
-	pass	
+func game_end(player_name): #TODO: Determin the winner and end the game
+	$KickoffUI.show()
+	show_message(player_name)
+	Vars.P1Score = 0
+	Vars.P2Score = 0
+	
 
 func _on_Player1Net_body_entered(_body: Node) -> void:
 	$Enviornment/score.play()
@@ -22,6 +32,7 @@ func _on_Player1Net_body_entered(_body: Node) -> void:
 	$KickoffUI.show()
 	Vars.velocity = Vector2(0, 0)
 	$Ball.position = Vector2(950, 420)
+	check_winner()
 	
 func _on_Player2Net_body_entered(_body: Node) -> void:
 	$Enviornment/score.play()
@@ -30,3 +41,14 @@ func _on_Player2Net_body_entered(_body: Node) -> void:
 	$KickoffUI.show()
 	Vars.velocity = Vector2(0, 0)
 	$Ball.position = Vector2(950, 420)
+	check_winner()
+	
+func check_winner():
+	if Vars.P1Score == 7:
+		game_end("Player 1 wins!")
+	elif Vars.P2Score == 7:
+		game_end("Player 2 wins!")
+
+func show_message(message):
+	$Message/MessageLabel.text = message
+	$Message/MessageLabel.show()
